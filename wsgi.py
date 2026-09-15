61fdcff30fc283af7e968a44fb462f3aa2a7d646
brayanherrera104 <brayan.herrera104@icloud.com>
2026-09-15 11:13:32 -0500

    OBRAX: load budget coverage module

--- wsgi.py
+++ wsgi.py
@@ -3,4 +3,5 @@ from app import app
 import superadmin  # noqa: F401,E402
 import control_obra  # noqa: F401,E402
 import budget_link  # noqa: F401,E402
+import budget_coverage  # noqa: F401,E402
 import dashboard_patch  # noqa: F401,E402
