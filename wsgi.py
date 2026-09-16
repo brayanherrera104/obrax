750f96f480bff561cfb66e64325fd82a220f33a1
brayanherrera104 <brayan.herrera104@icloud.com>
2026-09-15 22:07:42 -0500

    OBRAX 5.3 load plan capacity enforcement

--- wsgi.py
+++ wsgi.py
@@ -2,6 +2,7 @@
 from app import app
 import superadmin  # noqa: F401,E402
 import plans_v52  # noqa: F401,E402
+import plan_capacity  # noqa: F401,E402
 import control_obra  # noqa: F401,E402
 import budget_link  # noqa: F401,E402
 import budget_coverage  # noqa: F401,E402
